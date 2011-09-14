5f6f7026ff7d9aaa7538cfeacc3d0f51145f0112
Josh Wright <help@bendytree.com>
2011-09-13 22:28:53 -0500

    removed bloat from logging (for now)

--- Code/BT-Logger.h
+++ Code/BT-Logger.h
@@ -1,5 +1,11 @@
 #ifdef DEBUG
-#   define Log(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#   define XLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define XLog(...)
+#endif
+
+#ifdef DEBUG
+#   define Log NSLog
 #else
 #   define Log(...)
 #endif
